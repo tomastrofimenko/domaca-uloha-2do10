a46dc34ea539548b399f74fbad1ea17524c99cca
tomastrofimenko <145691836+tomastrofimenko@users.noreply.github.com>
2023-10-01 15:31:13 +0000

    .

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 #napiste program, ktory prevedie cislo z dvojkovej do desiatkovej sustavy
 x=int(input("Zadaj cislo: "))
 vysledok=0
-mocdes=1
+moc=1
 while x !=0:
   zvysok = x % 10 
-  vysledok = vysledok + zvysok * mocdes 
-  mocdes= mocdes*2
+  vysledok = vysledok + zvysok * moc 
+  moc= moc*2
   x = x // 10 
 print(vysledok)
